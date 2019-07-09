fa6543179a8a135ef4ebfa52e66e85356f33c629
TheGreekCuban <theodorealexander92@gmaill.com>
2019-07-09 16:24:15 -0400

    finished spotify portion, need to fix URL

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('This is loaded (:')
-
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -2,21 +2,19 @@ require("dotenv").config()
 const keys = require("./keys.js")
 const axios = require("axios")
 const moment = require("moment")
-// console.log(keys)
-// const spotify = new spotify(keys.spotify)
+const Spotify = require('node-spotify-api')
+const spotify = new Spotify(keys.spotify)
+
 
 let command = process.argv[2]
-let descriptor = process.argv[3]
-console.log("Command: ", command)
-console.log("Descriptor: ", descriptor)
+let descriptor = process.argv.slice(3).join(" ")
 
 /* 
 concert-this
------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
     syntax: node liri.js concert-this <artist/band name here>
 
-    This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-        
+    This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:    
         Name of the venue
         Venue location
         Date of the Event (use moment to format this as "MM/DD/YYYY")
@@ -36,7 +34,7 @@ const concertThis = () => {
 
 /*
 spotify-this-song
------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
     node liri.js spotify-this-song '<song name here>'
     This will show the following information about the song in your terminal/bash window:
         Artist(s)
@@ -45,24 +43,29 @@ spotify-this-song
         The album that the song is from
 
     If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-    You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-
-    The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-
-    Step One: Visit https://developer.spotify.com/my-applications/#!/
-
-    Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-    Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-    Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
 */
+
+const spotifyThisSong = () => {
+    
+    if(!descriptor) descriptor = "The Sign" 
+
+    spotify.search({type: 'track', query: descriptor, limt: 1})
+        .then(response => {
+            console.log("Artist: ", response.tracks.items[0].album.artists[0].name)
+            console.log("Title: ", descriptor)
+            //come back to URL to fix
+            console.log("Link: ", response.tracks.items[0].album.external_urls.spotify)
+            console.log("Album: ", response.tracks.items[0].album.name)
+            console.log("======================================")
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 /*
 movie-this
------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+----------------------------------------------------------------------------------------------------------------------------------
     node liri.js movie-this '<movie name here>'
 
     This will output the following information to your terminal/bash window:
@@ -86,7 +89,7 @@ movie-this
 
 /*
 do-what-it-says
------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
@@ -115,4 +118,4 @@ switch(command.toLowerCase()) {
                                                                     movie-this
                                                                     do-what-it-says`
         )
-    }
+}
